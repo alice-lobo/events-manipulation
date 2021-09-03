924439018875dfbbf577f46d58c48905f1cd1a9e
Alice <alicelobinha2@gmail.com>
2021-09-03 12:01:42 -0300

    finalizado

---
+++ src/AppOLD.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+
+
+
+
+const App = () => {
+
+  const name = 'Digital Innovation One'
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    console.log(value)
+
+  }
+
+  const showEvent = () => {
+    console.log('Evento clicado');
+    alert(name);
+  }
+
+  const Button = 
+    <button onClick={showEvent}>
+      Mostrar Evento
+    </button>
+
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      <input onChange={handleChange} />
+      {Button}
+    </div>
+  );
+};
+export default App;
